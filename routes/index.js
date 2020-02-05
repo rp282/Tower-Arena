a9aedab024022be4797753ac570151ad3c240d40
Ryan <ryanpun282@gmail.com>
2020-02-05 00:10:39 -0800

    dash

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('welcome');
-})
+// Welcome Page
+router.get('/', (req, res) =>  res.render('welcome'));
+// Dashboard
+router.get('dashboard', (req, res) => res.render('dashboard'));
 
 module.exports = router;
