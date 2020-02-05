a76149452a5f08527cac5ea2c5de788e7dd18b87
Ryan <ryanpun282@gmail.com>
2020-02-05 00:43:18 -0800

    dash

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const { ensureAuthenticated } = require('../config/auth');
 // Welcome Page
 router.get('/', (req, res) =>  res.render('welcome'));
 // Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+  console.log(req.user);
   res.render('dashboard', {
     user: req.user
-  })
-);
+  });
+});
 
 module.exports = router;
